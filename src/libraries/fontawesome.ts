d62d644f5977ca4557c3799a3ec0c8a48e24a91e
Hassan Sani <saniyhassan@gmail.com>
2021-06-14 15:14:50 +0100

    seperated fontawesome in library

---
+++ src/libraries/fontawesome.ts
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faClock, faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faClock,
+  faCloudSunRain
+)
