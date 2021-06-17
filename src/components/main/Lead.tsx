d6bd9587cd440e8b2a112d75ec6e5ae4028925b0
Hassan Sani <saniyhassan@gmail.com>
2021-06-17 19:50:44 +0100

    implementing lead sections

---
+++ src/components/main/Lead.tsx
@@ -0,0 +1,9 @@
+function Lead(page?: string): JSX.Element {
+  return (
+    <section>
+      Leads for most pages {page}
+    </section>
+  )
+}
+
+export default Lead
--- src/module/Home.tsx
+++ src/module/Home.tsx
@@ -1,7 +1,10 @@
+import Lead from "../components/main/Lead";
+
 function Home(): JSX.Element {
   return (
     <main className="w-full flex flex-col justify-start items-start min-h-screen">
       Home Show
+      <Lead></Lead>
     </main>
   );
 }
