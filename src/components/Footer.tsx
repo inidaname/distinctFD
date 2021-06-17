d93fb0a33d6f818285f72b385667b7f6b6b097bd
Hassan Sani <saniyhassan@gmail.com>
2021-06-17 19:39:05 +0100

    edited footer element

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Footer(): JSX.Element {
   return (
-    <footer>
-      Footer Here
+    <footer className="w-full h-60 flex flex-col py-5 justify-around bg-gradient-to-t from-gray-700 via-gray-700 to-gray-700">
+      <div className="flex flex-row justify-center items-center h-full mx-auto w-5/6">
+        <section className="flex flex-col justify-start items-start p-4 w-1/3 mx-2 h-full">
+          <img src="" className="h-16 w-full" alt="" />
+          <p className="text-sm text-white font-light p-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+            quidem architecto fugiat doloribus soluta dolore
+          </p>
+          <div className="flex flex-row justify-center items-center h-full w-full text-right">
+            <FontAwesomeIcon
+              className="py-1 text-2xl"
+              icon={["fab", "facebook"]}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className="py-1 text-2xl"
+              icon={["fab", "twitter"]}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className="py-1 text-2xl"
+              icon={["fab", "instagram"]}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className="py-1 text-2xl"
+              icon={["fab", "youtube"]}
+            ></FontAwesomeIcon>
+          </div>
+        </section>
+        <section className="flex flex-col justify-start items-start p-4 w-1/3 mx-2 h-full">
+          <h5 className="mb-4">Tags</h5>
+          <hr className="w-full" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illo officia aspernatur debitis doloribus! Quis, quos? </p>
+        </section>
+        <section className="flex flex-col justify-start items-start p-4 w-1/3 mx-2 h-full">
+          <h5 className="mb-4">Contact Us Through</h5>
+          <hr className="w-full" />
+        </section>
+      </div>
     </footer>
   );
 }
