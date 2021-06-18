60e4e989795b75fa1115730176a46af95eb90d4b
Hassan Sani <saniyhassan@gmail.com>
2021-06-18 02:01:23 +0100

    add hooks for fetch

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData]: [
+    any[] | undefined,
+    React.Dispatch<React.SetStateAction<any[] | undefined>>
+  ] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abort = new AbortController();
+    fetch(url, { signal: abort.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not load data for that response");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log(`Fetch Aborted`);
+        } else {
+          setError(err.message);
+        }
+      });
+
+    return () => abort.abort();
+  }, [url]);
+
+  return { error, data };
+};
+
+export default useFetch;
