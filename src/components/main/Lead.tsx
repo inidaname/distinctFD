bfe4e918a3822ed4cfb4337798a955a33b357009
Hassan Sani <saniyhassan@gmail.com>
2021-06-19 18:30:46 +0100

    integrating news api

--- src/components/main/Lead.tsx
+++ src/components/main/Lead.tsx
@@ -1,55 +1,48 @@
 
-function Lead({page, data, getRandom}: {page: string, data: {author: string, url: string}[], getRandom(): {url: string, author: string} }): JSX.Element {
-  
-  // function getRandom(): {url: string, author: string} {
-  //   let random: number = 0
-  //   if (data) {
-  //     random = Math.floor(Math.random() * data.length);
-  //     return (data) ? {url: data[random].download_url, author: data[random].author} : {url: '', author: ''};
-  //   }
-  //   return {url: '', author: ''}
-  // }
-
-  let error: boolean = (data.length === 0) ? true : false
+function Lead({page, data, isPending, error}: {page: string, data: any[] | undefined, isPending: boolean, error: null }): JSX.Element {
 
 
   return (
     <section className="h-96 mx-auto gap-2 grid-rows-2 grid-cols-5 grid w-5/6">
       <div className="relative row-span-3 col-span-3">
         <div className="absolute text-white flex flex-col justify-end p-6 z-20 h-full w-full bg-opacity-40 bg-black top-0 left-0">
-          <h3 className="font-bold">{data[0].author}</h3>
+          {isPending && <h3 className="font-bold">Loading</h3>}
+          <h3 className="font-bold">{data && data[0].author}</h3>
           <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eligendi quidem voluptatem et, aspernatur optio nesciunt obcaecati mollitia autem
+            Lorem ipsum dolor sit amet consectetur
           </p>
         </div>
-        <img src={data[0].url} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
+        <img src={data && data[0].download_url} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
       </div>
       <div className="relative">
         <div className="absolute text-white flex flex-col justify-end p-6 z-20 h-full w-full bg-opacity-40 bg-black top-0 left-0">
-          <h3 className="font-bold">{data[1].author}</h3>
+          {isPending && <h3 className="font-bold">Loading</h3>}
+          <h3 className="font-bold">{data && data[1].author}</h3>
             <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur
             </p>
         </div>
-        <img src={data[1].url} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
+        <img src={data && data[1].download_url} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
       </div>
       <div className="relative">
         <div className="absolute text-white flex flex-col justify-end p-6 z-20 h-full w-full bg-opacity-40 bg-black top-0 left-0">
-          <h3 className="font-bold">{data[2].author}</h3>
+          {isPending && <h3 className="font-bold">Loading</h3>}
+          <h3 className="font-bold">{data && data[2].author}</h3>
             <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur
             </p>
         </div>
-        <img src={data[2].url} className="z-10 absolute top-0 left-0 w-full h-full" alt="" />
+        <img src={data && data[2].download_url} className="z-10 absolute top-0 left-0 w-full h-full" alt="" />
       </div>
       <div className="relative row-span-2 col-span-2">
         <div className="absolute text-white flex flex-col justify-end p-6 z-20 h-full w-full bg-opacity-40 bg-black top-0 left-0">
-          <h3 className="font-bold">{data[3].author}</h3>
+          {isPending && <h3 className="font-bold">Loading</h3>}
+          <h3 className="font-bold">{data && data[3].author}</h3>
             <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur
             </p>
         </div>
-        <img src={`${data[3].url}/?grayscale&blur=2`} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
+        <img src={`${data && data[3].download_url}/?grayscale&blur=2`} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
       </div>
     </section>
   )
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 
 const useFetch = () => {
   const url = `https://picsum.photos/v2/list?page=1&limit=20`;
+  console.log('fetch call')
   const [data, setData]: [
     any[] | undefined,
     React.Dispatch<React.SetStateAction<any[] | undefined>>
   ] = useState();
   const [error, setError] = useState(null);
+  const [isPending, setIsPending]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(true)
 
   useEffect(() => {
     const abort = new AbortController();
@@ -18,12 +20,14 @@ const useFetch = () => {
         return res.json();
       })
       .then((data) => {
+        setIsPending(false);
         setData(data);
       })
       .catch((err) => {
         if (err.name === "AbortError") {
           console.log(`Fetch Aborted`);
         } else {
+          setIsPending(false);
           setError(err.message);
         }
       });
@@ -31,7 +35,7 @@ const useFetch = () => {
     return () => abort.abort();
   }, [url]);
 
-  return { error, data };
+  return { isPending, error, data };
 };
 
 export default useFetch;
