ca82583be25813b7b210ceb14a53aff9f98c9f00
Hassan Sani <saniyhassan@gmail.com>
2021-07-11 07:07:09 +0100

    time age implemented

--- src/components/main/Lead.tsx
+++ src/components/main/Lead.tsx
@@ -1,8 +1,11 @@
 import { ReactElement } from "react";
 
-function Lead({page, data: topFive, isPending, error}: {page: string, data: any[], isPending: boolean, error: null }): ReactElement {
+const timeLaps = new Intl.RelativeTimeFormat(navigator.language, {
+  style: "long",
+  numeric: "always"
+});
 
- 
+function Lead({page, data: topFive, isPending, error}: {page: string, data: any[], isPending: boolean, error: null }): ReactElement {
 
   return (
     <div className="w-5/6 mx-auto">
@@ -19,7 +22,7 @@ function Lead({page, data: topFive, isPending, error}: {page: string, data: any[
             <div className="absolute text-white flex flex-col justify-end p-6 z-20 h-full w-full bg-opacity-40 bg-black top-0 left-0">
               {isPending && <h3 className="font-bold">Loading</h3>}
               <h3 className="font-bold">{(tops.author) ? tops.author : tops.source.name}</h3>
-              <p className="text-sm">{tops.description} <br />{tops.publishedAt}</p>
+              <p className="text-sm">{tops.description} <br />{timeLaps.format(- new Date(tops.publishedAt).getHours(), 'hours')}</p>
             </div>
             <img src={tops.urlToImage} className=" z-10 absolute top-0 left-0 w-full h-full" alt="" />
           </div>
