e428103bd5becef8adfd207bc468185d8052346c
Hassan Sani <saniyhassan@gmail.com>
2021-09-13 09:10:34 +0100

    Add graphql for API calls

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,6 +20,26 @@ import storage from './reducers/store'
 import './libraries/fontawesome';
 
 const store: Store<any> = createStore(storage)
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+// const client = ...
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
 
 
 ReactDOM.render(
